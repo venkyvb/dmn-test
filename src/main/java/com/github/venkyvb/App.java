38aa99c6693b953cc4a1965d05c026587744b8aa
vbalakri <vbalakrishnan@linkedin.com>
2020-11-23 22:27:55 -0800

    new test with validity dates

--- src/main/java/com/github/venkyvb/App.java
+++ src/main/java/com/github/venkyvb/App.java
@@ -1,136 +1,155 @@
 package com.github.venkyvb;
 
+import java.text.ParseException;
 import java.util.*;
 
 import org.camunda.bpm.engine.variable.Variables;
 import org.camunda.bpm.model.dmn.HitPolicy;
 
 public final class App {
-    private App() {
-    }
+        private App() {
+        }
 
-    public static void main(String[] args) {
-        testRuleSet1();
-    }
+        public static void main(String[] args) throws ParseException {
+                testRuleSet1();
+                testRuleSet2();
+        }
 
+        private static String setUpRuleSet2(String ruleSetId) {
 
-    private static String setUpRuleSet1(String ruleSetId) {
+                List<DecisionTableInput> decisionTableInputs = new LinkedList<>();
+                decisionTableInputs.add(new DecisionTableInput("urn_type", "urn_type", "string", "Type"));
+                decisionTableInputs.add(new DecisionTableInput("urn_validity", "urn_validity", "date", "Validity"));
 
-        // 
-        List<DecisionTableInput> decisionTableInputs = new LinkedList<>();
-        decisionTableInputs.add(new DecisionTableInput("urn_type", "urn_type", "string", "Type"));
-        decisionTableInputs.add(new DecisionTableInput("urn_family", "urn_family", "string", "Family"));
-        decisionTableInputs.add(new DecisionTableInput("urn_term", "urn_term", "integer", "Term")); 
-        
-        List<DecisionTableOutput> decisionTableOutputs = new LinkedList<>();
-        decisionTableOutputs.add(new DecisionTableOutput("Discount", "discount", "double"));
+                List<DecisionTableOutput> decisionTableOutputs = new LinkedList<>();
+                decisionTableOutputs.add(new DecisionTableOutput("Discount", "discount", "double"));
 
+                List<RuleEntry> rules = new LinkedList<>();
 
-        List<RuleEntry> rules = new LinkedList<>();
+                List<String> inputValues1 = new LinkedList<>();
+                inputValues1.add("\"LNPRF\"");
+                inputValues1.add("[date and time(\"2020-10-01T00:00:00\")..date and time(\"2020-12-31T00:00:00\")]");
+                rules.add(new RuleEntry(inputValues1, "50"));
 
-        List<String> inputValuesLnprf = new LinkedList<>();
-        inputValuesLnprf.add("\"LNPRF\"");
-        inputValuesLnprf.add("\"LITS\",\"LILS\"");
-        inputValuesLnprf.add("");
-        rules.add(new RuleEntry(inputValuesLnprf, "50"));
+                List<String> inputValues2 = new LinkedList<>();
+                inputValues2.add("\"LNPRF\"");
+                inputValues2.add("[date and time(\"2020-01-01T00:00:00\")..date and time(\"2020-09-30T00:00:00\")]");
+                rules.add(new RuleEntry(inputValues2, "25"));
 
-        List<String> inputValuesLiss1 = new LinkedList<>();
-        inputValuesLiss1.add("");
-        inputValuesLiss1.add("\"LISS\"");
-        inputValuesLiss1.add(">35");
-        rules.add(new RuleEntry(inputValuesLiss1, "7.5"));
+                DecisionTableMetadata metadata = new DecisionTableMetadata(ruleSetId, decisionTableInputs,
+                                decisionTableOutputs, HitPolicy.UNIQUE);
 
-        List<String> inputValuesLiss2 = new LinkedList<>();
-        inputValuesLiss2.add("");
-        inputValuesLiss2.add("\"LISS\"");
-        inputValuesLiss2.add("(23..35]");
-        rules.add(new RuleEntry(inputValuesLiss2, "5"));      
-        
-        List<String> inputValuesLiss3 = new LinkedList<>();
-        inputValuesLiss3.add("");
-        inputValuesLiss3.add("\"LISS\"");
-        inputValuesLiss3.add("<=23");
-        rules.add(new RuleEntry(inputValuesLiss3, "3"));    
+                String dmnModel = DmnModelHandler.transform(metadata, rules);
 
-        DecisionTableMetadata metadata = new DecisionTableMetadata(ruleSetId, decisionTableInputs, decisionTableOutputs, HitPolicy.UNIQUE);
-             
-        String dmnModel = DmnModelHandler.transform(metadata, rules);
+                return dmnModel;
 
-        return dmnModel;
+        }
 
-    }
+        private static void testRuleSet2() throws ParseException {
 
-    private static void testRuleSet1() {
+                String ruleSetId = "testRuleSet2";
+                String dmnModel = setUpRuleSet2(ruleSetId);
 
-        String ruleSetId = "testRuleSet1";
-        String dmnModel = setUpRuleSet1(ruleSetId);
+                System.out.println(dmnModel);
 
-//        System.out.println(dmnModel);
-
-        System.out
-                .println(
-                        DmnModelHandler
-                                .evaluateRules(
-                                        ruleSetId, dmnModel,
-                                        Variables.createVariables().putValue("urn_type", "LNPRF")
-                                                .putValue("urn_family", "LITS").putValue("urn_term", 12))
+                System.out.println(DmnModelHandler
+                                .evaluateRules(ruleSetId, dmnModel,
+                                                Variables.createVariables().putValue("urn_type", "LNPRF")
+                                                                .putValue("urn_validity", "2020-10-01T00:00:00"))
                                 .toString());
 
-        System.out.println(DmnModelHandler
-                        .evaluateRules(ruleSetId, dmnModel,
-                                        Variables.createVariables().putValue("urn_type", "LNPRF")
-                                                        .putValue("urn_family", "LILS").putValue("urn_term", 12))
-                        .toString());      
-                        
-        System.out.println(DmnModelHandler
-                        .evaluateRules(ruleSetId, dmnModel,
-                                        Variables.createVariables().putValue("urn_type", "LNPRF")
-                                                        .putValue("urn_family", "LISS").putValue("urn_term", 12))
-                        .toString());                        
+                System.out.println(DmnModelHandler
+                                .evaluateRules(ruleSetId, dmnModel,
+                                                Variables.createVariables().putValue("urn_type", "LNPRF")
+                                                                .putValue("urn_validity", "2020-09-30T00:00:00"))
+                                .toString());
+        }
 
-        System.out
-                .println(
-                        DmnModelHandler
-                                .evaluateRules(
-                                        ruleSetId, dmnModel,
-                                        Variables.createVariables().putValue("urn_type", "LISS")
-                                                .putValue("urn_family", "LISS").putValue("urn_term", 12))
-                                .toString());   
-                                
-        System.out
-                .println(
-                        DmnModelHandler
-                                .evaluateRules(
-                                        ruleSetId, dmnModel,
-                                        Variables.createVariables().putValue("urn_type", "LISS")
-                                                .putValue("urn_family", "ABC").putValue("urn_term", 12))
-                                .toString());  
-                                
-        System.out
-                .println(
-                        DmnModelHandler
-                                .evaluateRules(
-                                        ruleSetId, dmnModel,
-                                        Variables.createVariables().putValue("urn_type", "LISS")
-                                                .putValue("urn_family", "LISS").putValue("urn_term", 26))
-                                .toString());        
-                                
-        System.out
-                .println(
-                        DmnModelHandler
-                                .evaluateRules(
-                                        ruleSetId, dmnModel,
-                                        Variables.createVariables().putValue("urn_type", "LISS")
-                                                .putValue("urn_family", "LISS").putValue("urn_term", 36))
-                                .toString());   
-                                
-        System.out
-                .println(
-                        DmnModelHandler
-                                .evaluateRules(
-                                        ruleSetId, dmnModel,
-                                        Variables.createVariables().putValue("urn_type", "ABC")
-                                                .putValue("urn_family", "LISS").putValue("urn_term", 36))
-                                .toString());                                
-    }
+        private static String setUpRuleSet1(String ruleSetId) {
+
+                //
+                List<DecisionTableInput> decisionTableInputs = new LinkedList<>();
+                decisionTableInputs.add(new DecisionTableInput("urn_type", "urn_type", "string", "Type"));
+                decisionTableInputs.add(new DecisionTableInput("urn_family", "urn_family", "string", "Family"));
+                decisionTableInputs.add(new DecisionTableInput("urn_term", "urn_term", "integer", "Term"));
+
+                List<DecisionTableOutput> decisionTableOutputs = new LinkedList<>();
+                decisionTableOutputs.add(new DecisionTableOutput("Discount", "discount", "double"));
+
+                List<RuleEntry> rules = new LinkedList<>();
+
+                List<String> inputValuesLnprf = new LinkedList<>();
+                inputValuesLnprf.add("\"LNPRF\"");
+                inputValuesLnprf.add("\"LITS\",\"LILS\"");
+                inputValuesLnprf.add("");
+                rules.add(new RuleEntry(inputValuesLnprf, "50"));
+
+                List<String> inputValuesLiss1 = new LinkedList<>();
+                inputValuesLiss1.add("");
+                inputValuesLiss1.add("\"LISS\"");
+                inputValuesLiss1.add(">35");
+                rules.add(new RuleEntry(inputValuesLiss1, "7.5"));
+
+                List<String> inputValuesLiss2 = new LinkedList<>();
+                inputValuesLiss2.add("");
+                inputValuesLiss2.add("\"LISS\"");
+                inputValuesLiss2.add("(23..35]");
+                rules.add(new RuleEntry(inputValuesLiss2, "5"));
+
+                List<String> inputValuesLiss3 = new LinkedList<>();
+                inputValuesLiss3.add("");
+                inputValuesLiss3.add("\"LISS\"");
+                inputValuesLiss3.add("<=23");
+                rules.add(new RuleEntry(inputValuesLiss3, "3"));
+
+                DecisionTableMetadata metadata = new DecisionTableMetadata(ruleSetId, decisionTableInputs,
+                                decisionTableOutputs, HitPolicy.UNIQUE);
+
+                String dmnModel = DmnModelHandler.transform(metadata, rules);
+
+                return dmnModel;
+
+        }
+
+        private static void testRuleSet1() {
+
+                String ruleSetId = "testRuleSet1";
+                String dmnModel = setUpRuleSet1(ruleSetId);
+
+                System.out.println(dmnModel);
+
+                System.out.println(DmnModelHandler.evaluateRules(ruleSetId, dmnModel, Variables.createVariables()
+                                .putValue("urn_type", "LNPRF").putValue("urn_family", "LITS").putValue("urn_term", 12))
+                                .toString());
+
+                System.out.println(DmnModelHandler.evaluateRules(ruleSetId, dmnModel, Variables.createVariables()
+                                .putValue("urn_type", "LNPRF").putValue("urn_family", "LILS").putValue("urn_term", 12))
+                                .toString());
+
+                System.out.println(DmnModelHandler.evaluateRules(ruleSetId, dmnModel, Variables.createVariables()
+                                .putValue("urn_type", "LNPRF").putValue("urn_family", "LISS").putValue("urn_term", 12))
+                                .toString());
+
+                System.out.println(DmnModelHandler.evaluateRules(ruleSetId, dmnModel, Variables.createVariables()
+                                .putValue("urn_type", "LISS").putValue("urn_family", "LISS").putValue("urn_term", 12))
+                                .toString());
+
+                System.out.println(DmnModelHandler
+                                .evaluateRules(ruleSetId, dmnModel,
+                                                Variables.createVariables().putValue("urn_type", "LISS")
+                                                                .putValue("urn_family", "ABC").putValue("urn_term", 12))
+                                .toString());
+
+                System.out.println(DmnModelHandler.evaluateRules(ruleSetId, dmnModel, Variables.createVariables()
+                                .putValue("urn_type", "LISS").putValue("urn_family", "LISS").putValue("urn_term", 26))
+                                .toString());
+
+                System.out.println(DmnModelHandler.evaluateRules(ruleSetId, dmnModel, Variables.createVariables()
+                                .putValue("urn_type", "LISS").putValue("urn_family", "LISS").putValue("urn_term", 36))
+                                .toString());
+
+                System.out.println(DmnModelHandler.evaluateRules(ruleSetId, dmnModel, Variables.createVariables()
+                                .putValue("urn_type", "ABC").putValue("urn_family", "LISS").putValue("urn_term", 36))
+                                .toString());
+        }
 }
